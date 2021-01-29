812bb25b9da1ce02703d77d71ab6b40e10bc9f6a
Jack Green <59716766+jasekk99@users.noreply.github.com>
2021-01-29 02:59:50 +0100

    Add search function

--- departure.php
+++ departure.php
@@ -7,6 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="Bahnhofsuhr/station-clock.js"></script>
     <script type="text/javascript" src="Bahnhofsuhr/draw.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <title>BVB Abfahrten</title>
 </head>
@@ -19,6 +20,20 @@
         <!--<canvas id="clock" width="150" height="150">
             Fehlermeldung       
         </canvas>-->
+        <div class="container" style="margin-left: 15px; padding-left: 30px;">
+            <form action="" method="post">
+            <input type="text" id="StationSearch" name="StationSearch">
+            <input type="submit" id="SubmitBtn">
+        </div>
+    </form>
+    <?php 
+    
+    if(isset($_POST['StationSearch'])){
+        $station = "?station=".$_POST['StationSearch'];
+        header("Location: ./departure.php".$station);
+    }
+    
+    ?>
     </div>
 
 
